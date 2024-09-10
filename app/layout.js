1dfdf866282d4723c0d1d876e0ad239afc4bd1f2
cristian-corbalan <ccorbalandw@gmail.com>
2024-09-10 15:36:09 -0300

    Install Semistandard linter and fix problems

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,10 @@ import './globals.css';
 
 export const metadata = {
   title: 'NextLevel Food',
-  description: 'Delicious meals, shared by a food-loving community.',
+  description: 'Delicious meals, shared by a food-loving community.'
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
     <html lang="en">
       <body>

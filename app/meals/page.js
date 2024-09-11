f6c3e5aac8742c264ee123507307addf9c77ef34
cristian-corbalan <ccorbalandw@gmail.com>
2024-09-11 16:13:33 -0300

    Show loader while fetching meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,23 @@
+import classes from '@/app/meals/page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import MealsHeader from '@/components/meals/meals-header';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals () {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals}/>;
+}
 
 export default function MealsPage () {
-  const meals = getMeals();
-
   return (
     <>
       <MealsHeader/>
       <main>
-        <MealsGrid meals={meals}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );

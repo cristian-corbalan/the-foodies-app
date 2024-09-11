87f1734fbb87da654b4da7fe57f8c012816ce5a2
cristian-corbalan <ccorbalandw@gmail.com>
2024-09-11 15:00:50 -0300

    Change logo link href

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -11,7 +11,7 @@ export default function MainHeader () {
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link className={classes.logo} href="/public">
+        <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="A plate with food on it" priority />
           NextLevel Food
         </Link>

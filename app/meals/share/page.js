0ae55d3e94d0b68c7f78e2e54b6cab5e6f052a38
cristian-corbalan <ccorbalandw@gmail.com>
2024-09-18 18:10:19 -0300

    Manage form submission status

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import FormSubmit from '@/components/forms/form-submit';
 import ImagePicker from '@/components/forms/image-picker';
 import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
@@ -42,7 +43,7 @@ export default function ShareMealPage () {
           </p>
           <ImagePicker label="Choose an image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <FormSubmit type="submit">Share Meal</FormSubmit>
           </p>
         </form>
       </main>
